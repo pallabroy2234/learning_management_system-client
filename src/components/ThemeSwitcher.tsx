afc8e880f822b388388ba658c3d92e1efb4e7b26
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-01-31 02:15:01 +0600

    Create ThemeSwitcher for theme switch

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,25 +1,56 @@
 import {useTheme} from "next-themes";
 import {useEffect, useState} from "react";
+import {BiMoon, BiSun} from "react-icons/bi";
 
-export default function ThemeSwitcher() {
-	const {theme, setTheme} = useTheme();
+
+const ThemeSwitcher = () => {
 	const [mounted, setMounted] = useState(false);
+	const {theme, setTheme} = useTheme();
 
-	// Ensure the theme is correctly set after the component mounts
+
 	useEffect(() => {
 		setMounted(true);
 	}, []);
 
 	if (!mounted) {
-		return null; // Prevents hydration mismatch in SSR
+		return null;
 	}
 
 	return (
-		<button
-			className="p-2 border rounded-md dark:bg-gray-800 bg-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700 transition"
-			onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-		>
-			{theme === "dark" ? "🌙 Dark Mode" : "☀️ Light Mode"}
-		</button>
+		<div className="flex items-center justify-center mx-4">
+			{
+				theme === "light" ? (
+					<BiMoon onClick={() => setTheme("dark")} fill="black" size={25} className="cursor-pointer" />
+				) : (
+					<BiSun onClick={() => setTheme("light")} size={25} className="cursor-pointer" />
+				)
+			}
+		</div>
 	);
-}
+};
+export default ThemeSwitcher;
+
+
+// export default function ThemeSwitcher() {
+//	
+// 	const {theme, setTheme} = useTheme();
+// 	const [mounted, setMounted] = useState(false);
+//
+// 	// Ensure the theme is correctly set after the component mounts
+// 	useEffect(() => {
+// 		setMounted(true);
+// 	}, []);
+//
+// 	if (!mounted) {
+// 		return null; // Prevents hydration mismatch in SSR
+// 	}
+//
+// 	return (
+// 		<button
+// 			className="p-2 border rounded-md dark:bg-gray-800 bg-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700 transition"
+// 			onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+// 		>
+// 			{theme === "dark" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+// 		</button>
+// 	);
+// }
