8fa969ac9f685eec803202b5c39d69b0614651a3
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-01-31 02:15:23 +0600

    Create Layout.tsx file for layout

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,8 +1,14 @@
 import {Outlet} from "react-router-dom";
+import Header from "../components/Header.tsx";
+import {useState} from "react";
 
 const Layout = () => {
+	const [open, setOpen] = useState(false);
+	const [activeItem, setActiveItem] = useState(0);
+
 	return (
 		<div>
+			<Header open={open} activeItem={activeItem} setOpen={setOpen} />
 			<Outlet />
 		</div>
 	);
