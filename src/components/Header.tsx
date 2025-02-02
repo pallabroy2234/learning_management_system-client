df98350a8271174e76a684e8ca7ed30e37061cf4
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-02 22:06:41 +0600

    small device navbar design finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ type Props = {
 const Header: FC<Props> = ({activeItem, setOpen}) => {
 	const [showHeader, setShowHeader] = useState(true);
 	const lastScrollY = useRef(0);
-	const [openSidebar, setOpenSidebar] = useState(false);
+	const [openSidebar, setOpenSidebar] = useState(true);
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -29,59 +29,63 @@ const Header: FC<Props> = ({activeItem, setOpen}) => {
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, []);
 
-	const handleSideBarClose = (e: any) => {
-		if (e.target.id === "screen") {
-			setOpenSidebar(false);
+
+	useEffect(() => {
+		if (openSidebar) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "";
 		}
-	};
+	}, [openSidebar]);
 
 	return (
-		<header
-			className={`fixed top-0 left-0 w-full h-[80px] z-[80] border-b
+		<>
+			<header
+				className={`fixed top-0 left-0 w-full h-[80px] z-[80] border-b
 				transition-transform duration-300 backdrop-blur-lg shadow-md
 				${showHeader ? "translate-y-0" : "-translate-y-full"}
 				bg-gradient-to-b from-white/30 to-white/10 dark:from-[#0a192f]/60 dark:to-[#172a45]/30
 				border-gray-200 dark:border-gray-700`}
-		>
-			<div className="w-[95%] 800px:w-[92%] mx-auto py-2 h-full flex items-center justify-between">
-				{/* Logo */}
-				<Link to="/" className="text-[25px] font-Poppins font-[500] text-black dark:text-white">
-					E-Learning
-				</Link>
+			>
+				<div className="w-[95%] 800px:w-[92%] mx-auto py-2 h-full flex items-center justify-between">
+					{/* Logo */}
+					<Link to="/" className="text-[25px] font-Poppins font-[500] text-black dark:text-white">
+						E-Learning
+					</Link>
 
-				{/* Navigation */}
-				<div className="flex items-center gap-4">
-					<NavItems activeItem={activeItem} isMobile={false} />
-					<ThemeSwitcher />
+					{/* Navigation */}
+					<div className="flex items-center gap-4">
+						<NavItems activeItem={activeItem} isMobile={false} />
+						<ThemeSwitcher />
 
-					{/* Mobile Menu */}
-					<div className="800px:hidden">
-						<HiOutlineMenuAlt3 onClick={() => setOpenSidebar(true)} size={25} className="cursor-pointer dark:text-white text-black" />
-					</div>
-
-					{/* User Profile */}
-					<HiUserCircle onClick={() => setOpen(true)} size={30} className="cursor-pointer hidden 800px:block dark:text-white text-black" />
-				</div>
-			</div>
-
-			{/* --------------------- Small Screen Sidebar----------------------    */}
-			{
-				openSidebar && (
-					<div>
-						<div onClick={handleSideBarClose} id="screen" className="fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]">
-							<div className="w-[70%] fixed z-[9999999] h-screen bg-white dark:bg-slate-900 dark:opacity-90 top-0 right-0">
-								<NavItems activeItem={activeItem} isMobile={true} />
-								<HiOutlineUserCircle className="cursor-pointer ml-5 my-6 text-black dark:text-white" size={25} />
-								<br />
-								<br />
-								<p className="text-[16px] px-2 pl-5 text-black dark:text-white">© 2025 Copyright: Pallab Roy Tushar</p>
-							</div>
+						{/* Mobile Menu */}
+						<div className="800px:hidden">
+							<HiOutlineMenuAlt3 onClick={() => setOpenSidebar(true)} size={25} className="cursor-pointer dark:text-white text-black" />
 						</div>
-						{/*<div className="fixed bg-black blur-md opacity-[0.3] top-0 left-0 bottom-0 right-0 z-[99998]"></div>*/}
+
+						{/* User Profile */}
+						<HiUserCircle onClick={() => setOpen(true)} size={30} className="cursor-pointer hidden 800px:block dark:text-white text-black" />
 					</div>
-				)
-			}
-		</header>
+				</div>
+			</header>
+
+			{/*--------------------- Small Screen Sidebar----------------------*/}
+			<div className={`fixed   bg-white dark:bg-slate-900  z-[99999] right-0 top-0 bottom-0 opacity-90 shadow-lg transition-all duration-300  ease-in-out ${openSidebar ? "translate-x-0" : "translate-x-full"}`}>
+				{
+					openSidebar && (
+						<>
+							<NavItems activeItem={activeItem} isMobile={true} />
+							<HiOutlineUserCircle className="cursor-pointer ml-5 my-6 text-black dark:text-white" size={25} />
+							<br />
+							<br />
+							<p className="text-[16px] px-2 pl-5 text-black dark:text-white">© 2025 Copyright: Pallab Roy Tushar</p>
+						</>
+					)
+				}
+			</div>
+			<div onClick={() => setOpenSidebar(false)} className={`absolute cursor-pointer top-0 right-0 bottom-0 left-0 overflow-hidden bg-black/50 opacity-50 inset-0 z-[99998] backdrop-blur-lg ${openSidebar ? "visible" : "invisible"}`}>
+			</div>
+		</>
 	);
 };
 
