8838c5bbfd33686f4b12e8a3923283a66bb698b5
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-06 23:40:32 +0600

    Login, Signup and verification state define

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -5,10 +5,11 @@ import {useState} from "react";
 const Layout = () => {
 	const [open, setOpen] = useState(false);
 	const [activeItem, setActiveItem] = useState(0);
+	const [route, setRoute] = useState("Login");
 
 	return (
 		<div>
-			<Header open={open} activeItem={activeItem} setOpen={setOpen} />
+			<Header open={open} activeItem={activeItem} setOpen={setOpen} route={route} setRoute={setRoute} setActiveItem={setActiveItem} />
 			<Outlet />
 		</div>
 	);
