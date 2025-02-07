84bb895e18eadf1ce4d24722b0e990e89225db90
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-08 02:49:40 +0600

    feat: config store in main file

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import "./index.css";
 import App from "./App.tsx";
 import ThemeProvider from "./context/ThemeProvider.tsx";
 import {Toaster} from "react-hot-toast";
+import {Provider} from "react-redux";
+import store from "./store/store.ts";
 
 
 createRoot(document.getElementById("root")!).render(
-	<ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-		<App />
-		<Toaster reverseOrder={false} />
-	</ThemeProvider>
+	<Provider store={store}>
+		<ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+			<App />
+			<Toaster reverseOrder={false} />
+		</ThemeProvider>
+	</Provider>
 );
