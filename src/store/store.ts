7b3febb40c156d1ffbb71656b275df1a78231b16
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-08 02:45:02 +0600

    config redux-toolkit store

---
+++ src/store/store.ts
@@ -0,0 +1,47 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {createLogger} from "redux-logger";
+import {api} from "./features/api.ts";
+
+
+// Node env
+const NODE_ENV = import.meta.env.VITE_NODE_ENV as string | "development";
+
+
+// config logger
+const logger = createLogger({
+	collapsed: true,
+	duration: true,
+	timestamp: true,
+	level: "log",
+	colors: {
+		title: () => "#139BFE",
+		prevState: () => "#1C5FAF",
+		action: () => "#149945",
+		nextState: () => "#A47104",
+		error: () => "#FF0005"
+	}
+});
+
+
+const store = configureStore({
+	reducer: {
+		// 	 API,
+		[api.reducerPath]: api.reducer
+		// 	Reducer
+	},
+	devTools: import.meta.env.VITE_NODE_ENV === "development",
+	middleware: (getDefaultMiddleware) => {
+		const middleware = getDefaultMiddleware().concat([
+			// 	 middleware
+			api.middleware
+		]);
+
+		if (NODE_ENV === "development") {
+			middleware.push(logger);
+		}
+		return middleware;
+	}
+});
+
+
+export default store;
