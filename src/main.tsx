8e9142aba798e60be08360428fcd4458248fb731
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-07 20:22:08 +0600

    add Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import {createRoot} from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import ThemeProvider from "./context/ThemeProvider.tsx";
+import {Toaster} from "react-hot-toast";
 
 
 createRoot(document.getElementById("root")!).render(
 	<ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-		<App  />
+		<App />
+		<Toaster reverseOrder={false} />
 	</ThemeProvider>
 );
