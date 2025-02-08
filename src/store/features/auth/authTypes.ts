52fde796ed8842611e847bad7ea149b825de85a0
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-08 22:54:44 +0600

    feat: create authTypes.ts for auth related all types define here

---
+++ src/store/features/auth/authTypes.ts
@@ -0,0 +1,33 @@
+export interface RegistrationRequest {
+	name: string,
+	email: string,
+	password: string
+}
+
+
+export interface RegistrationSuccessResponse {
+	success: true,
+	message: string,
+	activationCode: string
+}
+
+
+export interface ErrorResponse {
+	success: false,
+	message: string,
+}
+
+export type RegistrationResponse = RegistrationSuccessResponse | ErrorResponse;
+
+
+export interface ActivationRequest {
+	activation_token: string,
+	activation_code: string
+}
+
+export interface ActivationSuccessResponse {
+	success: true,
+	message: string
+}
+
+export type ActivationResponse = ActivationSuccessResponse | ErrorResponse;
