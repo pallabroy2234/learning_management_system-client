cd7ede4628e16073e6a6c2ad6962462846556706
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-09 02:56:54 +0600

    feat: implement verification functionality and some ui changes

--- src/components/auth/Verification.tsx
+++ src/components/auth/Verification.tsx
@@ -1,99 +1,12 @@
-// import {FC, useState, useRef} from "react";
-// import {VscWorkspaceTrusted} from "react-icons/vsc";
-//
-// type Props = {
-// 	setRoute: (route: string) => void;
-// }
-//
-//
-// type VerifyNumber = {
-// 	"0": string;
-// 	"1": string;
-// 	"2": string;
-// 	"3": string;
-// }
-//
-// const Verification: FC<Props> = ({setRoute}) => {
-// 	const [invalidError, setInvalidError] = useState<boolean>(false);
-// 	const [verifyNumber, setVerifyNumber] = useState<VerifyNumber>({
-// 		"0": "",
-// 		"1": "",
-// 		"2": "",
-// 		"3": ""
-// 	});
-//
-//
-// 	const inputRefs = [
-// 		useRef<HTMLInputElement>(null),
-// 		useRef<HTMLInputElement>(null),
-// 		useRef<HTMLInputElement>(null),
-// 		useRef<HTMLInputElement>(null)
-// 	];
-//
-// 	const verificationHandler = async () => {
-// 		console.log("Verification");
-// 		setInvalidError(true)
-// 	};
-//
-// 	const handleChange = (index: number, value: string) => {
-// 		setInvalidError(false);
-// 		const newVerifyNumber = {...verifyNumber, [index]: value};
-// 		setVerifyNumber(newVerifyNumber);
-// 		if (value === "" && index < 0) {
-// 			inputRefs[index - 1].current?.focus();
-// 		} else if (value.length === 1 && index < 3) {
-// 			inputRefs[index + 1].current?.focus();
-// 		}
-// 	};
-//
-// 	return (
-// 		<div>
-// 			<h1 className="text-[17px] 500px:text-[20px]  text-black dark:text-white font-[500] font-Poppins text-center py-2">Verify Your Account</h1>
-// 			<br />
-// 			<div className="w-full flex items-center justify-center mt-2">
-// 				<div className="w-[80px] h-[80px]  rounded-full bg-blue-500 flex items-center justify-center">
-// 					<VscWorkspaceTrusted size={40} className="text-white" />
-// 				</div>
-// 			</div>
-// 			<br />
-// 			<br />
-// 			<div className="m-auto flex items-center justify-around">
-// 				{
-// 					Object.keys(verifyNumber).map((key, index) => (
-// 						<input
-// 							type="number"
-// 							key={key}
-// 							ref={inputRefs[index]}
-// 							className={`w-[65px] h-[65px] bg-transparent border-[3px] rounded-[10px] flex items-center text-black dark:text-white justify-center text-[18px] font-Poppins outline-none text-center ${invalidError ? "animate-shake  border-red-500" : "dark:border-white border-[#0000004a]"}`}
-// 							placeholder=""
-// 							maxLength={1}
-// 							value={verifyNumber[key as keyof VerifyNumber]}
-// 							onChange={(e) => handleChange(parseInt(key), e.target.value)}
-// 						/>
-// 					))
-// 				}
-// 			</div>
-// 			<br />
-// 			<br />
-// 			<div className="w-full flex justify-center items-center">
-// 				<button onClick={verificationHandler} type="submit"
-// 						className={`w-full py-2 text-[13px] 500px:text-[16px] font-Poppins  rounded  transition-all duration-300 ease-in-out bg-blue-500 border-blue-500 text-white hover:bg-blue-600 hover:border-blue-600 shadow-md}`}
-// 				>
-// 					Verify OTP
-// 				</button>
-// 			</div>
-// 			<h5 className="text-center pt-4 font-Poppins text[14px] text-black dark:text-white">
-// 				Go back to sign in? <span onClick={() => setRoute("Login")} className="text-blue-500 pl-1 cursor-pointer">Sign in</span>
-// 			</h5>
-// 		</div>
-// 	);
-// };
-// export default Verification;
-
-
-import {FC, KeyboardEvent, useState} from "react";
+import {FC, KeyboardEvent, useEffect, useState} from "react";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {VscWorkspaceTrusted} from "react-icons/vsc";
+import {useActivationMutation} from "../../store/features/auth/authApi.ts";
+import {useSelector} from "react-redux";
+import {CustomError, RootState} from "../../types/@types.ts";
+import toast from "react-hot-toast";
+import {ActivationRequest} from "../../store/features/auth/authTypes.ts";
+import {ThreeDots} from "react-loader-spinner";
 
 type Props = {
 	setRoute: (route: string) => void;
@@ -105,16 +18,49 @@ type VerifyNumber = {
 	"3": string;
 }
 
+
 const Verification: FC<Props> = ({setRoute}) => {
 	const [invalidError, setInvalidError] = useState<boolean>(false);
+	const [timeLeft, setTimeLeft] = useState<number>(120); // 2 minutes countdown
+	const [isExpired, setIsExpired] = useState<boolean>(false);
+	const {token} = useSelector((state: RootState) => state.auth);
+	const [activation, {isLoading, isSuccess, isError, error, data}] = useActivationMutation();
 	const {
 		register,
 		handleSubmit,
 		setValue,
 		setFocus,
 		formState: {errors}
-	} = useForm<VerifyNumber>();
+	} = useForm<VerifyNumber>({});
 
+
+	useEffect(() => {
+		if (timeLeft > 0) {
+			const timer = setInterval(() => {
+				setTimeLeft((prev) => prev - 1);
+			}, 1000);
+
+			return () => clearInterval(timer);
+		} else {
+			setIsExpired(true);
+			toast.error("Your token has expired. Please request a new one.");
+		}
+	}, [timeLeft]);
+
+	useEffect(() => {
+		if (isSuccess) {
+			const message = data?.message;
+			toast.success(message);
+			setRoute("Login");
+		}
+		if (isError) {
+			if ("data" in error) {
+				const err = error as CustomError;
+				toast.error(err?.data?.message || "Account activation failed");
+				setInvalidError(true);
+			}
+		}
+	}, [isSuccess, isError]);
 
 	const handleInputChange = (index: number, value: string) => {
 		const numericValue = value.replace(/\D/g, "");
@@ -142,11 +88,16 @@ const Verification: FC<Props> = ({setRoute}) => {
 	};
 
 
-	const onSubmit: SubmitHandler<VerifyNumber> = async (data) => {
-		const otp = Object.values(data).join("")
-		console.log(otp);
-		setInvalidError(true);
-
+	const onSubmit: SubmitHandler<VerifyNumber> = async (data): Promise<void> => {
+		const otp = Object.values(data).join("");
+		try {
+			await activation({
+				activation_token: token,
+				activation_code: otp
+			} as ActivationRequest).unwrap();
+		} catch (e: any) {
+			console.log(e.message);
+		}
 	};
 
 	return (
@@ -190,16 +141,38 @@ const Verification: FC<Props> = ({setRoute}) => {
 				<br />
 				<br />
 				<div className="w-full flex justify-center items-center">
+					{/*<button*/}
+					{/*	type="submit"*/}
+					{/*	className="w-full flex justify-center items-center py-2 text-[13px] 500px:text-[16px] font-Poppins rounded transition-all duration-300 ease-in-out bg-blue-500 border-blue-500 text-white hover:bg-blue-600 hover:border-blue-600 shadow-md"*/}
+					{/*>*/}
+					{/*	{isLoading ? <ThreeDots height="20" width="40" radius="9" color="#fff" /> : "Verify OTP"}*/}
+					{/*</button>*/}
 					<button
 						type="submit"
-						className="w-full py-2 text-[13px] 500px:text-[16px] font-Poppins rounded transition-all duration-300 ease-in-out bg-blue-500 border-blue-500 text-white hover:bg-blue-600 hover:border-blue-600 shadow-md"
+						disabled={isExpired || isLoading}
+						className={`w-full flex justify-center items-center py-2 text-[13px] 500px:text-[16px] font-Poppins rounded transition-all duration-300 ease-in-out 
+                  ${isExpired ? "bg-blue-300 border-blue-400 text-blue-100 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600 text-white shadow-md"}
+                  `}
 					>
-						Verify OTP
+						{isLoading ? <ThreeDots height="20" width="40" radius="9" color="#fff" /> : "Verify OTP"}
 					</button>
 				</div>
+
+				<h5 className="text-center text-[14px] font-Poppins text-black dark:text-white mt-2">
+					{isExpired ? (
+						<div className="flex flex-col ">
+							<span className="text-red-500">Your token has expired. Request a new one. </span>
+							<span>Go back to <span className="text-blue-500 cursor-pointer" onClick={() => setRoute("Sign-up")}>Sign Up</span></span>
+						</div>
+					) : (
+						<span className={timeLeft <= 30 ? "text-red-500" : "text-blue-500"}>
+            Token expires in {Math.floor(timeLeft / 60)}:{(timeLeft % 60).toString().padStart(2, "0")}
+        </span>
+					)}
+				</h5>
 			</form>
 			<h5 className="text-center pt-4 font-Poppins text[14px] text-black dark:text-white">
-				Go back to sign in?{" "}
+				Go back to sign in?
 				<span
 					onClick={() => setRoute("Login")}
 					className="text-blue-500 pl-1 cursor-pointer"
