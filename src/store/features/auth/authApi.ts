3217dd9ab7a4b8f1a6d2e5d5d91ecc6429dc7a6f
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-08 22:58:17 +0600

    feat: add authApi.ts for authentication API user Redux RTK Query

---
+++ src/store/features/auth/authApi.ts
@@ -0,0 +1,63 @@
+import {api} from "../api.ts";
+import {setErrorMessage, userRegistration} from "./authSlice.ts";
+import {ActivationRequest, ActivationResponse, RegistrationRequest, RegistrationResponse} from "./authTypes.ts";
+import {handleError} from "../../../utils/errorHandler.ts";
+
+
+export const authApi = api.injectEndpoints({
+	endpoints: (builder) => ({
+
+		/**
+		 * @description       Register a new user
+		 * @route             POST /user/register
+		 * @access            Public
+		 * */
+		register: builder.mutation<RegistrationResponse, RegistrationRequest>({
+			query: (data) => ({
+				url: "/user/register",
+				method: "POST",
+				credentials: "include",
+				body: data
+			}),
+			// transformResponse: (response: RegistrationResponse) => response,
+			// transformErrorResponse: (response: any) => ({
+			// 	success: false,
+			// 	message: response.data?.message || "Registration failed",
+			// 	error: {
+			// 		code: response.status.toString(),
+			// 		details: response.data?.errors
+			// 	}
+			// }),
+			onQueryStarted: async (_, {dispatch, queryFulfilled}) => {
+				try {
+					const {data} = await queryFulfilled;
+					if (data.success) {
+						dispatch(userRegistration({
+							token: data?.activationCode
+						}));
+					}
+				} catch (err: any) {
+					console.log(err.message);
+					dispatch(setErrorMessage(handleError(err, "Registration failed")));
+				}
+			}
+		}),
+
+		/**/
+		activation: builder.mutation<ActivationResponse, ActivationRequest>({
+			query: ({activation_token, activation_code}) => ({
+				url: "/user/activate-user",
+				method: "POST",
+				credentials: "include",
+				body: {
+					activation_token,
+					activation_code
+				}
+			})
+		})
+
+	}),
+	overrideExisting: false
+});
+
+export const {useRegisterMutation, useActivationMutation} = authApi;
