7f092e14c0df7f01f33ae9cd55236858e75c67a2
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-10 03:29:21 +0600

    feat: setOpen pass as props

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -15,7 +15,7 @@ const CustomModal: FC<Props> = ({open, setOpen, setRoute, component: Component})
 	return (
 		<Modal open={open} onClose={() => setOpen(false)} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
 			<Box className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[300px] 320px:w-[300px] 400px:w-[350px] 500px:w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
-				<Component setRoute={setRoute} />
+				<Component setRoute={setRoute} setOpen={setOpen} />
 			</Box>
 		</Modal>
 	);
