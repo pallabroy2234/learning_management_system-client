7540ca46a93b87fb46a55e1044f73f905707cfad
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-13 00:23:34 +0600

    Noting Changed here just use before @react-oauth/google and wrap googleOAuthProvider. Now comment it because don't need it now

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,15 @@ import {Provider} from "react-redux";
 import store from "./store/store.ts";
 
 
+// Client ID : 41402644360-fb0f4d009be7fh0tthjh6dnoq9mm2t73.apps.googleusercontent.com
+// Client :  secret: GOCSPX - l0ygVyoxQmGfWvKs0IQbzKKnqMjl;
+
 createRoot(document.getElementById("root")!).render(
 	<Provider store={store}>
 		<ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+			{/*<GoogleOAuthProvider clientId="41402644360-fb0f4d009be7fh0tthjh6dnoq9mm2t73.apps.googleusercontent.com">*/}
 			<App />
+			{/*</GoogleOAuthProvider>*/}
 			<Toaster reverseOrder={false} />
 		</ThemeProvider>
 	</Provider>
