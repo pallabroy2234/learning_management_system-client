b81372cf9e72003dea3a007d351d8c28cc363c39
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-15 00:26:07 +0600

    feat(AuthSuccess.tsx): create AuthSuccess.tsx component for if user successfully login through OAuth redirect here and show there success message

---
+++ src/page/AuthSuccess.tsx
@@ -0,0 +1,26 @@
+import {Link, useSearchParams} from "react-router-dom";
+import {MdVerified} from "react-icons/md";
+
+const AuthSuccess = () => {
+	const [queryParams] = useSearchParams();
+
+	const success = queryParams.get("success");
+
+	const message = success ? decodeURIComponent(success) : "Thank you for logging in";
+	return (
+		<div className="h-screen grid place-items-center">
+			<div className="">
+				<div className="flex justify-center items-center my-3">
+					<MdVerified size={100} />
+				</div>
+				<h1 className="text-base font-bold text-center mb-5">
+					{message}
+				</h1>
+				<div className="flex justify-center items-center mt-3">
+					<Link to={"/"} className="bg-transparent border-black dark:border-white text-black dark:text-white border px-8 py-1 rounded">Go Back</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default AuthSuccess;
