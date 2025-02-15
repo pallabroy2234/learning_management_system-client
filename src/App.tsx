d93cd0680518596d4b6b1c909b8d65cf229a06a4
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-15 18:01:56 +0600

    feat(App): implement unified loading with Suspense and user data integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Layout from "./Layout/Layout.tsx";
-import Home from "./page/Home.tsx";
-import AuthFailure from "./page/AuthFailure.tsx";
-import AuthSuccess from "./page/AuthSuccess.tsx";
-import {useEffect} from "react";
+import {useEffect, lazy, Suspense, useState} from "react";
 import store from "./store/store.ts";
-import {initializeAuth} from "./store/features/auth/authApi.ts";
+import {initializeAuth, useGetCurrentUserQuery} from "./store/features/auth/authApi.ts";
+import Loader from "./components/Loader.tsx";
+
+
+// * Import all Route components
+const Home = lazy(() => import("./page/Home.tsx"));
+const Layout = lazy(() => import("./Layout/Layout.tsx"));
+const AuthFailure = lazy(() => import("./page/AuthFailure.tsx"));
+const AuthSuccess = lazy(() => import("./page/AuthSuccess.tsx"));
 
 
 const App = () => {
+	const {isLoading} = useGetCurrentUserQuery(undefined, {skip: true});
+	const [loader, setLoader] = useState<boolean>(false);
+
+	// * Initialize authentication state on app load
 	useEffect(() => {
 		(async () => await initializeAuth(store))();
 	}, []);
 
-	return (
+	// * Show loader while fetching user data
+	useEffect(() => {
+		isLoading ? setLoader(true) : setLoader(false);
+	}, [isLoading]);
+
+
+	return loader ? (
+		<Loader />
+	) : (
 		<BrowserRouter>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route index={true} element={<Home />} />
-					<Route path="/auth/failure" element={<AuthFailure />} />
-					<Route path="/auth/success" element={<AuthSuccess />} />
-				</Route>
-			</Routes>
+			<Suspense fallback={<Loader />}>
+				<Routes>
+					<Route element={<Layout />}>
+						<Route index={true} element={<Home />} />
+						<Route path="/auth/failure" element={<AuthFailure />} />
+						<Route path="/auth/success" element={<AuthSuccess />} />
+					</Route>
+				</Routes>
+			</Suspense>
 		</BrowserRouter>
 	);
 };
