6837641575d77bdc505132431adb256240b72535
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-15 18:52:45 +0600

    feat(ProtectedRoute.tsx): create Protected Route and implement logic

---
+++ src/components/ProtecttedRoute/ProtectedRoute.tsx
@@ -0,0 +1,28 @@
+import {Navigate, Outlet} from "react-router-dom";
+import {FC, ReactElement} from "react";
+import {useSelector} from "react-redux";
+import {RootState} from "../../types/@types.ts";
+
+
+type Props = {
+	adminRoute?: boolean;
+	children?: ReactElement;
+	redirect?: string
+}
+
+const ProtectedRoute: FC<Props> = ({adminRoute, redirect = "/", children}) => {
+	const {user} = useSelector((state: RootState) => state.auth);
+
+
+	if (!user) {
+		return <Navigate to={redirect} />;
+	}
+
+
+	if (adminRoute && user.role !== "admin") {
+		return <Navigate to={redirect} />;
+	}
+
+	return children ? children : <Outlet />;
+};
+export default ProtectedRoute;
