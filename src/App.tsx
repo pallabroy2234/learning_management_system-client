4a979b0aeff92d45c8710abb7c609eaa720ccb2d
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-17 21:43:02 +0600

    feat(App.tsx): add profile Route and set Profile element

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {useEffect, lazy, Suspense, useState} from "react";
 import store from "./store/store.ts";
-import {initializeAuth, useGetCurrentUserQuery} from "./store/features/auth/authApi.ts";
+import {initializeAuth} from "./store/features/auth/authApi.ts";
 import Loader from "./components/Loader.tsx";
+import ProtectedRoute from "./components/ProtecttedRoute/ProtectedRoute.tsx";
+import "react-image-crop/dist/ReactCrop.css";
 
 
 // * Import all Route components
 const Home = lazy(() => import("./page/Home.tsx"));
+const Profile = lazy(() => import("./page/Profile.tsx"));
 const Layout = lazy(() => import("./Layout/Layout.tsx"));
 const AuthFailure = lazy(() => import("./page/AuthFailure.tsx"));
 const AuthSuccess = lazy(() => import("./page/AuthSuccess.tsx"));
 
 
 const App = () => {
-	const {isLoading} = useGetCurrentUserQuery(undefined, {skip: true});
-	const [loader, setLoader] = useState<boolean>(false);
+	const [loader, setLoader] = useState<boolean>(true);
 
-	// * Initialize authentication state on app load
+
 	useEffect(() => {
-		(async () => await initializeAuth(store))();
+		const initializeAuthState = async () => {
+			try {
+				setLoader(true);
+				await initializeAuth(store);
+			} finally {
+				setLoader(false);
+			}
+		};
+		initializeAuthState();
 	}, []);
-
-	// * Show loader while fetching user data
-	useEffect(() => {
-		isLoading ? setLoader(true) : setLoader(false);
-	}, [isLoading]);
 
 
 	return loader ? (
@@ -33,10 +38,21 @@ const App = () => {
 		<BrowserRouter>
 			<Suspense fallback={<Loader />}>
 				<Routes>
-					<Route element={<Layout />}>
-						<Route index={true} element={<Home />} />
-						<Route path="/auth/failure" element={<AuthFailure />} />
-						<Route path="/auth/success" element={<AuthSuccess />} />
+					<Route>
+						{/*  Frontend Routes */}
+						<Route element={<Layout />}>
+							<Route index={true} element={<Home />} />
+							<Route path="/auth/failure" element={<AuthFailure />} />
+							<Route path="/auth/success" element={<AuthSuccess />} />
+
+							{/*	Only Logged in user Routes */}
+							<Route element={<ProtectedRoute />}>
+								<Route path="/profile" element={<Profile />} />
+							</Route>
+						</Route>
+
+						{/*	 Dashboard */}
+
 					</Route>
 				</Routes>
 			</Suspense>
