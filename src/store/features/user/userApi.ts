a00d690527aca6649bdb5b7219c165279817f51a
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-17 22:49:54 +0600

    feat(userApi.ts): create rtk query userApi file for user related api define here

---
+++ src/store/features/user/userApi.ts
@@ -0,0 +1,27 @@
+import {api} from "../api.ts";
+
+
+export const uerApi = api.injectEndpoints({
+	endpoints: (builder) => ({
+
+		/**
+		 * @summary       Upload user profile image
+		 * @description   Upload user profile image
+		 * @method        POST
+		 * @path          /user/update-avatar
+		 * @security      Private
+		 * */
+		uploadProfileImage: builder.mutation({
+			query: (data) => ({
+				url: "/user/update-avatar",
+				method: "POST",
+				body: data,
+				credentials: "include"
+			}),
+			invalidatesTags: ["User"]
+		})
+	})
+});
+
+
+export const {useUploadProfileImageMutation} = uerApi;
