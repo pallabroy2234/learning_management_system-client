459e281998d12407b6c5e27ae7c219f57f897a3a
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-17 21:43:34 +0600

    refactor css

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -14,7 +14,7 @@ const CustomModal: FC<Props> = ({open, setOpen, setRoute, component: Component})
 
 	return (
 		<Modal open={open} onClose={() => setOpen(false)} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-			<Box className="absolute left-[50%] 800px:top-[40%] top-[30%] translate-x-[-50%] translate-y-[-50%] w-[300px] 320px:w-[300px] 400px:w-[350px] 500px:w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
+			<Box className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[300px] 320px:w-[300px] 400px:w-[350px] 500px:w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
 				<Component setRoute={setRoute} setOpen={setOpen} />
 			</Box>
 		</Modal>
