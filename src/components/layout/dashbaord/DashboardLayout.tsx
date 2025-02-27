c6826738e87115b7ee9a7b0d93273580ac7c4df7
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-28 00:35:59 +0600

    feat: update DashboardLayout to integrate AdminSidebar with show state and remove unused SmallDeviceAdminSidebar

--- src/components/layout/dashbaord/DashboardLayout.tsx
+++ src/components/layout/dashbaord/DashboardLayout.tsx
@@ -1,24 +1,19 @@
 import {Outlet} from "react-router-dom";
-
 import {useState} from "react";
 import AdminSidebar from "./AdminSidebar.tsx";
 import DashboardHeader from "./DashboardHeader.tsx";
-import SmallDeviceAdminSidebar from "./SmallDeviceAdminSidebar.tsx";
 
 
 const DashboardLayout = () => {
-    const [isCollapsed, setIsCollapsed] = useState(true)
     const [show, setShow] = useState(false)
 
     return (
         <div className="w-[100%] h-[700px] overflow-x-hidden">
-            {/* SideBar */}
-            <AdminSidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed}/>
+            <DashboardHeader setShow={setShow}/>
+            <AdminSidebar show={show} setShow={setShow}/>
 
-            <DashboardHeader isCollapsed={isCollapsed} setShow={setShow}/>
 
-            <SmallDeviceAdminSidebar show={show} setShow={setShow}/>
-            <div className={`${isCollapsed ? "700px:w-[calc(100%-320px)] 700px:ml-[320px] w-[100%]" : "700px:w-[calc(100%-80px)]  700px:ml-[80px] w-[100%]"}  transition-[width margin-left]  duration-300`}>
+            <div>
                 <Outlet/>
             </div>
         </div>
