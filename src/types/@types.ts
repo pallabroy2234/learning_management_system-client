16d486f0a69d9ccac9ea60bd5db9861cb75e10c0
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-03-07 03:30:47 +0600

    feat: add ICourseFormValues interface to define course creation form structure

--- src/types/@types.ts
+++ src/types/@types.ts
@@ -1,29 +1,51 @@
 import store from "../store/store.ts";
 
 export type CustomError = {
-	status: number;
-	data: {
-		success: boolean;
-		message: string;
-		payload?: any;
-	};
+    status: number;
+    data: {
+        success: boolean;
+        message: string;
+        payload?: any;
+    };
 };
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export interface IUser {
-	_id: string;
-	name: string;
-	email: string;
-	password?: string;
-	avatar: {
-		url: string;
-		public_id: string;
-	};
-	role: string;
-	isVerified: boolean;
-	courses: Object[];
-	createdAt: string;
-	updatedAt: string;
-	__v: number;
+    _id: string;
+    name: string;
+    email: string;
+    password?: string;
+    avatar: {
+        url: string;
+        public_id: string;
+    };
+    role: string;
+    isVerified: boolean;
+    courses: object[];
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+}
+
+export interface ICourseFormValues {
+    thumbnail: string;
+    name: string;
+    price: number;
+    estimatedPrice: number;
+    tags: string;
+    level: string;
+    description: string;
+    demoUrl: string;
+    benefits: { title: string }[];
+    prerequisites: { title: string }[];
+    courseData: {
+        title: string;
+        videoDescription: string;
+        videoUrl: string;
+        videoSection: string;
+        videoLength: number | null;
+        videoPlayer: string;
+        links: { title: string; url: string }[];
+    }[];
 }
