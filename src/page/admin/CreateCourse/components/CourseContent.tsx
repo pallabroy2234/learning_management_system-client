2d2c10abd7110f1e55a72a1b285640d85a973b4d
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-03-14 19:28:45 +0600

    feat: refactor CourseContent component by removing unused props, simplifying animations, and enhancing error handling

--- src/page/admin/CreateCourse/components/CourseContent.tsx
+++ src/page/admin/CreateCourse/components/CourseContent.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from "react";
+import {useState} from "react";
 import {AnimatePresence, motion} from "framer-motion";
 import {FiInfo, FiTrash} from "react-icons/fi";
 import {useFieldArray, useFormContext} from "react-hook-form";
@@ -7,22 +7,20 @@ import {ICourseContentErrors} from "../../../../types/@types.ts";
 import {CgMathPlus} from "react-icons/cg";
 import CourseLinks from "./CourseLinks.tsx";
 
-interface IProps {
-	setStep: (step: number) => void;
-}
-
-const CourseContent: FC<IProps> = ({setStep}) => {
+const CourseContent = () => {
 	const [activeIndex, setActiveIndex] = useState<number | null | string>(0);
 
 	const {
 		register,
-		trigger,
 		control,
-		formState: {errors, isValid},
+		formState: {errors},
 	} = useFormContext();
 	const err = errors?.courseData as ICourseContentErrors;
 
-	// * ============================= Course Data Field Array ============================ * //
+	/**
+	 * @summary       React Hook Form Field Array
+	 * @description   This hook is used to manage the array of course data
+	 * */
 	const {
 		fields: courseDataField,
 		append: courseDataAppend,
@@ -33,18 +31,9 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 		shouldUnregister: false,
 	});
 
-	const handlePrevious = () => {
-		setStep(1);
-	};
-
-	const handleNext = async () => {
-		const isValid = await trigger();
-		if (isValid) {
-			setStep(3);
-		}
-	};
-
-	// * ============================= Animations ============================ * //
+	/**
+	 * @summary       Animation
+	 * */
 
 	const accordionAnimation = {
 		open: {
@@ -81,14 +70,14 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 			transition: {duration: 0.5},
 		};
 	};
-	const errorMessageAnimation = {
+	const errorTextAnimation = {
 		initial: {opacity: 0, x: 15},
 		animate: {opacity: 1, x: 0},
 		exit: {opacity: 0, x: -15},
 	};
 
 	return (
-		<motion.div {...initialAnimation}  className='1200px:w-[65%] 1000px:w-[90%]  w-[100%] mx-auto'>
+		<motion.div {...initialAnimation} className='1200px:w-[65%] 1000px:w-[90%]  w-[100%] mx-auto'>
 			{/* ----------------------- Heading ------------------------ */}
 			<motion.div className='400px:text-2xl text-lg font-bold mb-6 dark:text-white flex items-center'>
 				<FiInfo className='inline-block mr-2 text-purple-500 dark:text-purple-400 ' />
@@ -102,9 +91,9 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 					{courseDataField.map((item, index) => {
 						return (
 							<motion.div
-								initial={{opacity:0, y:15}}
-								animate={{opacity:1, y:0}}
-								exit={{opacity:0, x:-15}}
+								initial={{opacity: 0, y: 15}}
+								animate={{opacity: 1, y: 0}}
+								exit={{opacity: 0, x: -15}}
 								key={item.id}
 								className='flex flex-col gap-5'>
 								<div className='dark:bg-slate-600 bg-gray-100 flex justify-center items-center rounded-md'>
@@ -121,17 +110,13 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 
 									{/*  Trash Button */}
 									<div className='mr-3'>
-										{
-											courseDataField.length > 1 && (
-												<motion.button
-													onClick={() => courseDataRemove(index)}
-													type='button'>
-													<div className='bg-red-400 py-2 px-2 rounded-lg text-white'>
-														<FiTrash className='text-lg' />
-													</div>
-												</motion.button>
-											)
-										}
+										{courseDataField.length > 1 && (
+											<motion.button onClick={() => courseDataRemove(index)} type='button'>
+												<div className='bg-red-400 py-2 px-2 rounded-lg text-white'>
+													<FiTrash className='text-lg' />
+												</div>
+											</motion.button>
+										)}
 									</div>
 								</div>
 
@@ -155,7 +140,7 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 												className={`${err?.[index]?.title ? "border-red-500" : "dark:border-slate-500 border-gray-400"}  focus:outline-none px-3 py-[8px] dark:bg-transparent border rounded`}
 											/>
 											{err?.[index]?.title && (
-												<motion.div {...errorMessageAnimation} className='text-red-500 text-sm'>
+												<motion.div {...errorTextAnimation} className='text-red-500 text-sm'>
 													<p>{err?.[index]?.title?.message}</p>
 												</motion.div>
 											)}
@@ -173,7 +158,7 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 												className={`${err?.[index]?.videoSection ? "border-red-500" : "dark:border-slate-500 border-gray-400"}  focus:outline-none px-3 py-[8px] dark:bg-transparent border rounded`}
 											/>
 											{err?.[index]?.videoSection && (
-												<motion.div {...errorMessageAnimation} className='text-red-500 text-sm'>
+												<motion.div {...errorTextAnimation} className='text-red-500 text-sm'>
 													<p>{err?.[index]?.videoSection?.message}</p>
 												</motion.div>
 											)}
@@ -184,17 +169,17 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 									<div className='flex 800px:flex-row flex-col 800px:gap-6 gap-5'>
 										<div className='flex flex-col gap-2 w-full'>
 											<label htmlFor={`courseData${index}.videoUrl`} className='dark:text-slate-400 text-gray-600'>
-												Video URL <span className='text-red-500'>*</span>
+												Video ID <span className='text-red-500'>*</span>
 											</label>
 											<motion.input
 												{...register(`courseData.${index}.videoUrl`)}
 												{...errorInputAnimation(err?.[index]?.videoUrl)}
 												type='text'
-												placeholder='https://www.youtube.com'
+												placeholder='d28141d876b3470eb1577456e4b42846'
 												className={`${err?.[index]?.videoUrl ? "border-red-500" : "dark:border-slate-500 border-gray-400"}  focus:outline-none px-3 py-[8px] dark:bg-transparent border rounded`}
 											/>
 											{err?.[index]?.videoUrl && (
-												<motion.div {...errorMessageAnimation} className='text-red-500 text-sm'>
+												<motion.div {...errorTextAnimation} className='text-red-500 text-sm'>
 													<p>{err?.[index]?.videoUrl?.message}</p>
 												</motion.div>
 											)}
@@ -215,7 +200,7 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 												className={`${err?.[index]?.videoLength ? "border-red-500" : "dark:border-slate-500 border-gray-400"}  focus:outline-none px-3 py-[8px] dark:bg-transparent border rounded`}
 											/>
 											{err?.[index]?.videoLength && (
-												<motion.div {...errorMessageAnimation} className='text-red-500 text-sm'>
+												<motion.div {...errorTextAnimation} className='text-red-500 text-sm'>
 													<p>{err?.[index]?.videoLength?.message}</p>
 												</motion.div>
 											)}
@@ -233,11 +218,11 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 												{...register(`courseData.${index}.suggestion`)}
 												{...errorInputAnimation(err?.[index]?.suggestion)}
 												type='text'
-												placeholder='Practive more to better understand'
+												placeholder='Practice writing basic HTML structures'
 												className={`${err?.[index]?.suggestion ? "border-red-500" : "dark:border-slate-500 border-gray-400"}  focus:outline-none px-3 py-[8px] dark:bg-transparent border rounded`}
 											/>
 											{err?.[index]?.suggestion && (
-												<motion.div {...errorMessageAnimation} className='text-red-500 text-sm'>
+												<motion.div {...errorTextAnimation} className='text-red-500 text-sm'>
 													<p>{err?.[index]?.suggestion?.message}</p>
 												</motion.div>
 											)}
@@ -250,11 +235,11 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 												{...register(`courseData.${index}.videoPlayer`)}
 												{...errorInputAnimation(err?.[index]?.videoPlayer)}
 												type='text'
-												placeholder='Youtube, Vimeo, etc'
+												placeholder='VdoCipher, Vimeo, etc'
 												className={`${err?.[index]?.videoPlayer ? "border-red-500" : "dark:border-slate-500 border-gray-400"}  focus:outline-none px-3 py-[8px] dark:bg-transparent border rounded`}
 											/>
 											{err?.[index]?.videoPlayer && (
-												<motion.div {...errorMessageAnimation} className='text-red-500 text-sm'>
+												<motion.div {...errorTextAnimation} className='text-red-500 text-sm'>
 													<p>{err?.[index]?.videoPlayer?.message}</p>
 												</motion.div>
 											)}
@@ -272,7 +257,7 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 											className={`${err?.[index]?.videoDescription ? "border-red-500" : "dark:border-slate-500 border-gray-400"}  focus:outline-none px-3 py-[8px] dark:bg-transparent border rounded resize-none`}
 										/>
 										{err?.[index]?.videoDescription && (
-											<motion.div {...errorMessageAnimation} className='text-red-500 text-sm'>
+											<motion.div {...errorTextAnimation} className='text-red-500 text-sm'>
 												<p>{err?.[index]?.videoDescription?.message}</p>
 											</motion.div>
 										)}
@@ -311,29 +296,7 @@ const CourseContent: FC<IProps> = ({setStep}) => {
 					</motion.button>
 				</AnimatePresence>
 			</motion.div>
-
-			<div className='flex justify-between mt-10 items-center'>
-				<motion.button
-					onClick={handlePrevious}
-					whileHover={{scale: 1.05}}
-					whileTap={{scale: 0.9}}
-					type='button'
-					className={`bg-transparent 600px:px-[100px] 400px:px-[50px] px-8 py-2  rounded border border-blue-500 text-blue-500 dark:text-white 600px:text-base text-sm`}>
-					Previous
-				</motion.button>
-				<motion.button
-					disabled={!isValid}
-					onClick={handleNext}
-					whileHover={!isValid ? {scale: 1} : {scale: 1.05}}
-					whileTap={!isValid ? {scale: 1} : {scale: 0.95}}
-					type='button'
-					className={`${isValid ? "bg-blue-500 hover:bg-blue-600" : "bg-blue-400 cursor-not-allowed"}  600px:px-[100px] 400px:px-[50px] px-8 py-2  rounded text-white 600px:text-base text-sm`}>
-					Next
-				</motion.button>
-			</div>
 		</motion.div>
 	);
 };
 export default CourseContent;
-
-
