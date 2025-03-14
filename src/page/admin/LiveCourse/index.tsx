997acc4f17bb968c23477ac078367a26410ec462
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-03-15 00:08:08 +0600

    feat: add LiveCourse component and route for course management

---
+++ src/page/admin/LiveCourse/index.tsx
@@ -0,0 +1,11 @@
+import CourseCreationForm from "../CreateCourse/components/BenefitsStep.tsx";
+
+
+const LiveCourse = () => {
+    return (
+        <div className="mt-[100px]">
+            <CourseCreationForm/>
+        </div>
+    )
+}
+export default LiveCourse
