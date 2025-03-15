ac8b5c41aa0248579ab2435188f48dfecceee65e
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-03-16 03:12:48 +0600

    feat: implement course retrieval and display in LiveCourse component

--- src/page/admin/LiveCourse/index.tsx
+++ src/page/admin/LiveCourse/index.tsx
@@ -1,11 +1,53 @@
-import CourseCreationForm from "../CreateCourse/components/BenefitsStep.tsx";
-
+import {useEffect, useState} from "react";
+import CoursesTable from "./component/CoursesTable.tsx";
+import {useGetCoursesQuery} from "../../../store/features/course/courseApi.ts";
+import {CustomError} from "../../../types/@types.ts";
+import toast from "react-hot-toast";
+import Loader from "../../../components/shared/Loader.tsx";
 
 const LiveCourse = () => {
-    return (
-        <div className="mt-[100px]">
-            <CourseCreationForm/>
-        </div>
-    )
-}
-export default LiveCourse
+
+
+	const {data:getAllCourses, isLoading, isSuccess, isError, error} = useGetCoursesQuery({})
+
+
+	const [courses, setCourses] = useState<any>([]);
+
+
+
+	useEffect(()=> {
+		if(isSuccess){
+			setCourses(getAllCourses?.payload)
+		}
+		if(isError){
+			if("data" in error){
+				const err = error as CustomError;
+				const message = err.data.message || "An error occurred";
+				toast.error(message);
+			}
+		}
+	},[isSuccess, isError])
+	
+
+
+	// course: any
+	const handleEdit = () => {
+		// Implement edit logic
+	};
+
+	// courseId: string
+	const handleDelete = () => {
+		// Implement delete logic
+	};
+	return (
+		<div className='mt-[120px]'>
+			{/*<CoursesTable data={courses} onEdit={handleEdit} onDelete={handleDelete} />*/}
+			<div className='px-4 sm:px-6 min-h-screen mb-[80px]'>
+				{
+                       isLoading ? <Loader/> : <CoursesTable data={courses} onEdit={handleEdit} onDelete={handleDelete} />
+				}
+			</div>
+		</div>
+	);
+};
+export default LiveCourse;
