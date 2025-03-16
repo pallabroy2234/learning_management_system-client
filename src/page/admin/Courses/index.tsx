bd1544e980444f8c7072833647e86371a6b6273e
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-03-16 22:33:50 +0600

    feat: refactor Users and Courses components to use reusable Table component

--- src/page/admin/Courses/index.tsx
+++ src/page/admin/Courses/index.tsx
@@ -1,24 +1,34 @@
-import {useEffect, useState} from "react";
-import CoursesTable from "./component/CoursesTable.tsx";
+import {useEffect, useMemo} from "react";
 import {useGetCoursesQuery} from "../../../store/features/course/courseApi.ts";
 import {CustomError} from "../../../types/@types.ts";
 import toast from "react-hot-toast";
 import Loader from "../../../components/shared/Loader.tsx";
+import Table from "../../../components/shared/Table.tsx";
+import {ColumnDef} from "@tanstack/react-table";
+import {motion} from "framer-motion";
+import {FiEdit, FiTrash2} from "react-icons/fi";
+
+
+
+
+interface Course {
+	_id: string;
+	name: string;
+	description: string;
+	price: number;
+	level: string;
+	createdAt: string;
+	rating: number;
+	purchased: number;
+	thumbnail: {
+		url: string;
+	};
+}
 
 const LiveCourse = () => {
-
-
 	const {data:getAllCourses, isLoading, isSuccess, isError, error} = useGetCoursesQuery({})
 
-
-	const [courses, setCourses] = useState<any>([]);
-
-
-
 	useEffect(()=> {
-		if(isSuccess){
-			setCourses(getAllCourses?.payload)
-		}
 		if(isError){
 			if("data" in error){
 				const err = error as CustomError;
@@ -26,25 +36,121 @@ const LiveCourse = () => {
 				toast.error(message);
 			}
 		}
-	},[isSuccess, isError])
-	
+	},[isSuccess, isError, error])
 
 
-	// course: any
-	const handleEdit = () => {
-		// Implement edit logic
+
+
+
+	const truncate = (text: string, length: number) => {
+		return text.length > length ? text.substring(0, length) + "..." : text;
 	};
 
-	// courseId: string
-	const handleDelete = () => {
-		// Implement delete logic
-	};
+	const columns = useMemo<ColumnDef<Course>[]>(
+		() => [
+			// {
+			// 	id: "Select",
+			// 	header: ({table})=> (
+			// 		<input type="checkbox" checked={table.getIsAllRowsSelected()} onChange={table.getToggleAllPageRowsSelectedHandler()}  className="form-checkbox h-4 w-4 rounded text-purple-600 transition focus:ring-purple-500 dark:bg-gray-700"/>
+			// 	),
+			// 	cell: ({row})=> (
+			// 		<input
+			// 			checked={row.getIsSelected()}
+			// 			onChange={row.getToggleSelectedHandler()}
+			// 			type="checkbox"
+			// 			className="form-checkbox h-4 w-4 rounded text-purple-500 transition focus:ring-purple-500 dark:bg-gray-700"
+			// 		/>
+			// 	)
+			// },
+			{
+				accessorKey: "_id",
+				header: "ID",
+			},
+			{
+				accessorKey: "thumbnail",
+				header: "Thumbnail",
+				cell: ({row}) => (
+					<div className=''>
+						<img src={row.original.thumbnail.url} alt={row.original.name} className='w-[120px] h-[50px] ' />
+					</div>
+				),
+			},
+			{
+				accessorKey: "name",
+				header: "Course Name",
+				cell: ({getValue}) => <span className='font-medium text-gray-800 dark:text-gray-200'>{truncate(getValue<string>(), 50)}</span>,
+			},
+			{
+				accessorKey: "description",
+				header: "Description",
+				cell: ({getValue}) => <span className='max-w-xs  text-gray-600 dark:text-gray-400'>{truncate(getValue<string>(), 30)}</span>,
+			},
+			{
+				accessorKey: "price",
+				header: "Price",
+				cell: ({getValue}) => `$${getValue<number>().toLocaleString()}`,
+			},
+			{
+				accessorKey: "level",
+				header: "Level",
+				cell: ({getValue}) => (
+					<span className='inline-block rounded-full bg-purple-500 px-3 py-1 text-sm text-white dark:bg-purple-900/30 dark:text-purple-400'>
+						{getValue<string>()}
+					</span>
+				),
+			},
+			{
+				accessorKey: "createdAt",
+				header: "Created Data",
+				cell: ({getValue}) => new Date(getValue<string>()).toLocaleDateString(),
+			},
+			{
+				accessorKey: "rating",
+				header: "Rating",
+				cell: ({getValue}) => (
+					<div className='flex items-center'>
+						<span className='mr-2 text-yellow-500'>★</span>
+						{getValue<number>()}
+					</div>
+				),
+			},
+			{
+				accessorKey: "purchased",
+				header: "Purchased",
+			},
+			{
+				id: "Actions",
+				header: "Actions",
+				cell: () => (
+					<div className='flex space-x-3'>
+						<motion.button
+							type="button"
+							whileHover={{scale: 1.05}}
+							whileTap={{scale: 0.95}}
+							className='text-blue-500 hover:text-blue-600 dark:text-blue-400'>
+
+							<FiEdit size={18} />
+						</motion.button>
+
+						<motion.button
+							whileHover={{scale: 1.05}}
+							whileTap={{scale: 0.95}}
+							className='text-red-500 hover:text-red-600 dark:text-red-400'>
+							<FiTrash2 size={18} />
+						</motion.button>
+					</div>
+				),
+			},
+		],
+		[],
+	);
+
 	return (
 		<div className='mt-[120px]'>
 			{/*<CoursesTable data={courses} onEdit={handleEdit} onDelete={handleDelete} />*/}
 			<div className='px-4 sm:px-6 min-h-screen mb-[80px]'>
 				{
-                       isLoading ? <Loader/> : <CoursesTable data={courses} onEdit={handleEdit} onDelete={handleDelete} />
+                       isLoading ? <Loader/> : <Table data={isSuccess ? getAllCourses?.payload : []} columns={columns}  isDownload={true} />
 				}
 			</div>
 		</div>
--- src/page/admin/Users/index.tsx
+++ src/page/admin/Users/index.tsx
@@ -1,9 +1,30 @@
-import UserTable from "./components/UserTable.tsx";
-import {useEffect, useState} from "react";
-import {CustomError, IUser} from "../../../types/@types.ts";
+
+import {useEffect, useMemo, useState} from "react";
+import {CustomError} from "../../../types/@types.ts";
 import {useGetAllUsersByAdminQuery} from "../../../store/features/user/userApi.ts";
 import toast from "react-hot-toast";
 import Loader from "../../../components/shared/Loader.tsx";
+import Table from "../../../components/shared/Table.tsx";
+import {ColumnDef} from "@tanstack/react-table";
+import {motion} from "framer-motion";
+import {FiTrash} from "react-icons/fi";
+import {MdOutlineMail} from "react-icons/md";
+
+
+
+interface IUser {
+	_id: string;
+	avatar: {
+		url: string;
+	};
+	name: string;
+	email: string;
+	provider: string;
+	role: string;
+	isVerified: boolean;
+	courses: string[];
+	createdAt: string;
+}
 
 const Users = () => {
 	const [users, setUsers] = useState<Array<IUser>>([]);
@@ -25,12 +46,87 @@ const Users = () => {
 
 
 
+	const columns = useMemo<ColumnDef<IUser>[]>(
+		() => [
+			{
+				accessorKey: "_id",
+				header: "ID",
+				cell: ({getValue}) => <span className='text-sm text-gray-600 dark:text-gray-400'>{getValue<string>()}</span>,
+			},
+			{
+				accessorKey: "avatar",
+				header: "Avatar",
+				cell: ({row}) => <img src={row.original.avatar.url} alt={row.original.name} className='w-[50px] h-[50px] rounded-full ' />,
+			},
+			{
+				accessorKey: "name",
+				header: "Name",
+				cell: ({getValue}) => <span className='text-sm text-gray-600 dark:text-gray-400'>{getValue<string>()}</span>,
+			},
+			{
+				accessorKey: "email",
+				header: "Email",
+				cell: ({getValue}) => <span className='text-sm text-gray-600 dark:text-gray-400'>{getValue<string>()}</span>,
+			},
+			{
+				accessorKey: "role",
+				header: "Role",
+				cell: ({getValue}) => <span className='text-sm text-gray-600 dark:text-gray-400'>{getValue<string>()}</span>,
+			},
+			{
+				accessorKey: "isVerified",
+				header: "Verified",
+				cell: ({getValue}) => <span className='text-sm ml-4 text-gray-600 dark:text-gray-400'>{getValue<boolean>() ? "Yes" : "No"}</span>,
+			},
+			{
+				accessorKey: "createdAt",
+				header: "Joined",
+				cell: ({getValue}) => (
+					<span className='text-sm text-gray-600 dark:text-gray-400'>{new Date(getValue<string>()).toLocaleDateString()}</span>
+				),
+			},
+			{
+				accessorKey: "courses",
+				header: "Purchased Courses",
+				cell: ({getValue}) => (
+					<span className='text-sm flex ml-[60px]  items-center text-gray-600 dark:text-gray-400'>{getValue<string[]>().length}</span>
+				),
+			},
+			{
+				accessorKey: "actions",
+				header: "Actions",
+				cell: () => (
+					<div className='flex items-center  gap-2'>
+						<motion.button
+							whileHover={{scale: 0.9}}
+							whileTap={{scale: 1.05}}
+							type='button'
+							className='flex ml-4 text-red-500 dark:text-red-400'>
+							<FiTrash size={18} />
+						</motion.button>
+					</div>
+				),
+			},
+			{
+				accessorKey: "Email",
+				header: "Email",
+				cell: ({row}) => (
+					<motion.button onClick={() => window.location.href = `mailto:${row.original.email}`} whileHover={{scale: 0.9}} whileTap={{scale: 1.05}} type='button' className='ml-2'>
+						<MdOutlineMail size={20} />
+					</motion.button>
+				),
+			},
+		],
+		[],
+	);
+
+
 
 	return (
 		<div className='mt-[120px]'>
 			<div className='px-4 sm:px-6 min-h-screen mb-[80px]'>
 				{
-					isLoading ? <Loader/> : <UserTable data={users} />
+					isLoading ? <Loader/> : <Table data={users} columns={columns}  isHeader={true} placeholder="Search Users" headerContent="Users"/>
 				}
 			</div>
 		</div>
